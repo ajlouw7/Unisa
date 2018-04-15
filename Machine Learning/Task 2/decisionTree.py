45571db3cb846800f66163eb6cac137af7bd0239
LEVIATHAN\Riaan <ajlouw7@gmail.com>
2018-04-15 20:24:46 +0200

    ml update

--- Machine Learning/Task 2/decisionTree.py
+++ Machine Learning/Task 2/decisionTree.py
@@ -1,9 +1,50 @@
 import math
 
-p = input("Value of p\n")
-n = input("Value of n\n")
+class AttributeValue:
+    name = ""
+    p = 0
+    n = 0
 
-p = int(p)
-n = int(n)
-I = -p/(p+n)*math.log2(p/(p+n)) - n/(p+n)*math.log2(n/(n+p))
-print(I)
+
+def Entropy(num,denom):
+    if( num == 0):
+        return 0
+    return -num/denom*math.log2(num/denom)
+
+def CalcI(p,n):
+    p=int(p)
+    n=int(n)
+    return  Entropy(p,p+n) + Entropy(n,n+p)
+
+
+numberOValuesOfAttribute = input("Number of values of attribute")
+
+totalp = input("Get total number of p\n")
+totaln = input("Get total number of n\n")
+
+attributeValues = []
+for i in range(0, int(numberOValuesOfAttribute) ):
+    question = "Attribute value " + str(i+1) + ": "
+    name = input(question)
+    av = AttributeValue()
+    av.name = name
+    attributeValues.append( av )
+
+for i in range(0, int(numberOValuesOfAttribute) ):
+    av = attributeValues[i]
+    print("For " + av.name)
+    av.p = input("Value of p\n")
+    av.n = input("Value of n\n")
+
+IofPN = CalcI(totalp,totaln)
+
+totalCount = int(totalp) + int(totaln)
+E = 0
+
+for av in attributeValues:
+    E= E + (int(av.p)+int(av.n))/totalCount*CalcI(int(av.p),int(av.n))
+
+gain = IofPN - E
+print("I = " + str(IofPN))
+print("E = " + str(E))
+print("gain = " + str(gain))
