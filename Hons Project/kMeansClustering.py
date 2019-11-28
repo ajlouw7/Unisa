379a483ac75c0c2685ef85a1a7e6aa9a8f2c1585
LEVIATHAN\Riaan <ajlouw7@gmail.com>
2019-11-28 11:19:48 +0200

    clustering

--- Hons Project/kMeansClustering.py
+++ Hons Project/kMeansClustering.py
@@ -3,14 +3,23 @@ import numpy as np
 import Utils as utils
 import kNN as knn
 import kfold as kfold
+import OLS as ols
 
 class CentroidData:
-    centoid = []
-    nearestDataPoints = []
+    def __init__(self):
+        self.centoid = []
+        self.nearestDataPoints = []
+        #for performance
+        self.nearestDataPointsTempList = []
+        #testing data for centroid
+        self.testingData = []
+        #for performance
+        self.nearestDataPointsTempListForTestingData = []
 
-df = pd.read_csv("test.csv")
-x_df = df[['Health_Expenditure','GDP_Per_Capita','Education','Unemployment']]
-y_df = df[['Life_Expectancy']]
+class CentroidResults:
+    def __init__(self):
+        self.centroidData = []
+        self.olsRuns = []
 
 def randomN(df):
     return pd.DataFrame( np.random.rand(1,11), columns=df.columns)
@@ -40,7 +49,10 @@ def getClosestCentroid(datapoint,centroidDatas):
     closestCentroidData = []
     closestDistance = 9999999999.9
     for i in range(len(centroidDatas)):
-        distance = knn.distance(centroidDatas[i].centoid, datapoint)
+        #distance = knn.distance(centroidDatas[i].centoid, datapoint)
+        distance = utils.GetEuclideanDistanceBetweenFeatureVectors(kfold.getX(centroidDatas[i].centoid),kfold.getX(datapoint) )
+
+
         if( distance < closestDistance):
             closestCentroidData = centroidDatas[i]
             closestDistance = distance
@@ -63,20 +75,25 @@ def getMeanPoint(nearestDataPoints,trainingData):
 
 
 
-
-def CalculateCentroids(trainingData, k):
-    centroidDatas = getInitialCentroidDatas(k,trainingData)  
+def CalculateCentroids(trainingData, k, movingThreshold, centroidDatas):
     for j in range(100):
         #assign training data point to closest centroid
         for i in range(len(trainingData)):
             dataRow = trainingData.iloc[i]
             centroidData = getClosestCentroid( dataRow, centroidDatas)
-            centroidData.nearestDataPoints = centroidData.nearestDataPoints.append( dataRow, ignore_index=True )
+            centroidData.nearestDataPointsTempList.append(dataRow)
+            #centroidData.nearestDataPoints = centroidData.nearestDataPoints.append( dataRow, ignore_index=True,sort=False )
 
-        
+        numberOfCentroidsThatDidNotMove = 0
         for i in range(len(centroidDatas)):
+              #removing all nearest points
+            centroidDatas[i].nearestDataPoints.dropna()
+            centroidDatas[i].nearestDataPoints = pd.DataFrame(centroidDatas[i].nearestDataPointsTempList, columns=trainingData.columns)
+            # centroidDatas[i].nearestDataPoints = pd.concat( [centroidDatas[i].nearestDataPoints, centroidDatas[i].nearestDataPointsTempList])
+            #clear temp list 
+            centroidDatas[i].nearestDataPointsTempList = []
             newCentroid = getMeanPoint(centroidDatas[i].nearestDataPoints,trainingData)
-        
+           
         
         
         
@@ -86,22 +103,62 @@ def CalculateCentroids(trainingData, k):
             old = kfold.getX(centroidDatas[i].centoid)
             new = kfold.getX(newCentroid)
             diffrenceInCentroid = old - new 
-            print( "centroid difference" + str(i) )
+            distanceBetweenOldAndNewCentroid = utils.GetEuclideanDistanceBetweenFeatureVectors(kfold.getX(centroidDatas[i].centoid),kfold.getX(newCentroid) )
+            print( "centroid difference " + str(i) + ' distance= ' + str(distanceBetweenOldAndNewCentroid) )
             print( diffrenceInCentroid )
             #set new mean of data points as the new centroid
             centroidDatas[i].centoid = newCentroid
-            #removing all nearest points
-            centroidDatas[i].nearestDataPoints.dropna()
+            if( distanceBetweenOldAndNewCentroid < movingThreshold ):
+               numberOfCentroidsThatDidNotMove = numberOfCentroidsThatDidNotMove +1
+
+        #return if none of the centroids have moved
+        if( numberOfCentroidsThatDidNotMove >= k):
+            return centroidDatas
+    # return if a 100 itterations have been reached
     return centroidDatas
 
 
 k = 2
 
-cen = CalculateCentroids(kfold.testSet(1).trainingDF, k)
+#cen = CalculateCentroids(kfold.testSet(1).trainingDF, k)
 
 
+#trainingX = kfold.getX(ts.trainingDF).to_numpy()
+#trainingY = kfold.getY(ts.trainingDF).to_numpy()
+#testingX = kfold.getX(ts.testingDF).to_numpy()
+#testingY = kfold.getY(ts.testingDF).to_numpy()
 
 
+#results = ols.RunDataset( trainingY, trainingX, testingY, testingX)
+
+def RunClustering(testSet, k, movingThreshold):
+    centroidResults = CentroidResults()
+
+    centroidDatas = getInitialCentroidDatas(k,testSet.trainingDF)  
+
+    cen = CalculateCentroids(testSet.trainingDF, k, movingThreshold, centroidDatas)
+    
+    #associate testing data to centroids
+    for i in range(len(testSet.testingDF)):
+        dataRow = testSet.testingDF.iloc[i]
+        centroidData = getClosestCentroid( dataRow, centroidDatas)
+        centroidData.nearestDataPointsTempListForTestingData.append(dataRow)
+    
+    for i in range(k):
+        cen[i].testingData = pd.DataFrame(centroidDatas[i].nearestDataPointsTempListForTestingData, columns=testSet.testingDF.columns)
+
+        trainingX = kfold.getX(cen[i].nearestDataPoints).to_numpy()
+        trainingY = kfold.getX(cen[i].nearestDataPoints).to_numpy()
+        testingX = kfold.getX(cen[i].testingData).to_numpy()
+        testingY = kfold.getY(cen[i].testingData).to_numpy()
+        results = ols.RunDataset( trainingY, trainingX, testingY, testingX)
+        #store centroid data in results
+        centroidResults.centroidData.append(cen[i])
+        #store results of running ols on the test data for that centroid
+        centroidResults.olsRuns.append( results )
+    return centroidResults
+
+r = RunClustering(kfold.testSet(1), k, 0.1 )
 #l = getKCentroids(2)
 
 #a = randomN()
