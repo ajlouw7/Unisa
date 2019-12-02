96ba7ce774b83243efd9cf03a85c65e3155b7499
LEVIATHAN\Riaan <ajlouw7@gmail.com>
2019-12-02 18:01:49 +0200

    consolidated cluster results and added centroid data

--- Hons Project/Clustering-Run.py
+++ Hons Project/Clustering-Run.py
@@ -6,32 +6,39 @@ import csv
 
 #i=1
 #k=5
-movingThreshold= 0.001 
+movingThreshold= 0.001
 index = 0
 
 
 analysisdf = pd.DataFrame( columns=['Series','k','centroidNum','Mean','stdDev','count'])
 
-#results[centroidIndex].centroidData
-#results[centroidIndex].olsRuns.results
+#for each fold
 for i in range(1,11):
-    for k in range(2,11):
+    #for each number of clusters
+    for k in range(2,16):
         results = cluster.RunClustering(kfold.testSet(i), k, movingThreshold )
-        for centroidIndex in range(k):
-            fileName = 'Cluster-Results\Cluster_Results' + str(i) + 'cluster=' + str(centroidIndex +1) + 'of' + str(k) + ' theshold= ' + str(movingThreshold) + '.csv'
-            with open(fileName,mode='w', newline='') as resultsFile:
-                resultsWriter = csv.writer( resultsFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                resultsWriter.writerow(['LE','Predicted LE', 'Error','Health_Expenditure','GDP_Per_Capita','Education','Unemployment'])
+        #for each centroid    
+        fileName = 'Cluster-Results\Cluster_Results' + str(i) + '#ofclusters=' + str(k) + ' theshold= ' + str(movingThreshold) + '.csv'
+        with open(fileName,mode='w', newline='') as resultsFile:
+            resultsWriter = csv.writer( resultsFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            resultsWriter.writerow(['LE','Predicted LE', 'Error','Health_Expenditure','GDP_Per_Capita','Education','Unemployment','centroid#','centroid_Health_Expenditure','centroid_GDP_Per_Capita','centroid_Education','centroid_Unemployment'])
+            for centroidIndex in range(k):
+                he = results.centroidData[centroidIndex].centoid.at[0,'Health_Expenditure']
+                gdp = results.centroidData[centroidIndex].centoid.at[0,'GDP_Per_Capita']
+                edu = results.centroidData[centroidIndex].centoid.at[0,'Education']
+                unem = results.centroidData[centroidIndex].centoid.at[0,'Unemployment']
                 for r in results.olsRuns[centroidIndex].results:   
-                    resultsWriter.writerow([float(r.lifeExpectancy), float(r.predictedLifeExpectancy), float(r.error), r.inputFeatureVector[0], r.inputFeatureVector[1],r.inputFeatureVector[2],r.inputFeatureVector[3]])
-            df = pd.read_csv(fileName)
+                    resultsWriter.writerow([float(r.lifeExpectancy), float(r.predictedLifeExpectancy), float(r.error), r.inputFeatureVector[0], r.inputFeatureVector[1],r.inputFeatureVector[2],r.inputFeatureVector[3], str(centroidIndex + 1),float(he),float(gdp),float(edu),float(unem)])
+        df = pd.read_csv(fileName)
+        for centroidNum in range(1,k+1):
             index =  index +1
             analysisdf.at[index,'Series'] = i
             analysisdf.at[index,'k'] = k 
-            analysisdf.at[index,'centroidNum'] = centroidIndex + 1  
-            analysisdf.at[index,'Mean'] = df['Error'].mean()
-            analysisdf.at[index,'stdDev'] = df['Error'].std()
-            analysisdf.at[index,'count'] = df['Error'].count()
+            analysisdf.at[index,'centroidNum'] = centroidNum
+            filtereddf = df.where( df['centroid#'] == centroidNum )
+            analysisdf.at[index,'Mean'] = filtereddf['Error'].mean()
+            analysisdf.at[index,'stdDev'] = filtereddf['Error'].std()
+            analysisdf.at[index,'count'] = filtereddf['Error'].count()
 
 analysisdf.to_csv('Analysis\Cluster-Analysis.csv')
 l = 0
