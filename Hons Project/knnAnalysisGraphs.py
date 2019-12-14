624b2886dfd139e6839ac2fbc51a791170a40a41
LEVIATHAN\Riaan <ajlouw7@gmail.com>
2019-12-14 14:01:21 +0200

    update

--- Hons Project/knnAnalysisGraphs.py
+++ Hons Project/knnAnalysisGraphs.py
@@ -8,11 +8,18 @@ import seaborn as sns
 
 df = pd.read_csv('Analysis\\kNN-Analysis.csv')
 
-k=2
+k=10
 #for k in range(1,11):
+
 seriesData = df.loc[df['Series'] == k]
+#seriesData = df
+
+seriesData = seriesData.rename(columns={"Mean": "Mean Error in Predicted Life Expectancy"})
 
 fileName = "kNNErrorPlot\kNNErrorPlot" + str(k) + ".png"
-sns.lineplot( x='k', y='Mean', data=seriesData).get_figure().savefig(fileName)
+
+#fileName = "kNNErrorPlot\kNNErrorPlot.png"
+
+sns.lineplot( x='k', y='Mean Error in Predicted Life Expectancy',ci="sd", data=seriesData).get_figure().savefig(fileName)
 
 l=0
